349e8c574d6e92a13e7ef39e49b2c088e779962f
CRYPTIC HUNT <vaibhav.srivast123@gmail.com>
2021-07-19 12:02:05 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+ import './Header.css'
+function Header() {
+  return (
+    <div className='header'>
+       <img src="http://pngimg.com/uploads/amazon/amazon_PNG27.png"
+    </div>
+  )
+}
+
+export default Header
+
